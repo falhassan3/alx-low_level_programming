37596f5a219520e42a8d0ccf6fb1a695e5a7497d
falhassan3 <fareedalhassan65@gmail.com>
2022-10-16 06:48:26 -0700

    task 8 c progarmming

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * Main -Entry
+ * Return: 1
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar,
+2015-100-19\n", 59);
+return (1);
+}
