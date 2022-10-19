4d30cdb4d4fcaeb3e88718a47385d45588bf981c
falhassan3 <fareedalhassan65@gmail.com>
2022-10-19 06:12:48 -0700

    c programming task 6 fixed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 /**
  * main - Entry
- * [stderr]: [Anything]
  * Return: Always 0
  */
 int main(void)
 {
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
 
-	printf("Size of char: %li byte(s)\n", sizeof(char));
-	printf("Size of an int: %li byte(s)\n", sizeof(int));
-	printf("Size of a long int: %li byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %li byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %li byte(s)\n", sizeof(float));
+	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 
 }
