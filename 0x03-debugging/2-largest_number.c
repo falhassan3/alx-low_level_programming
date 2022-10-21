b7afdd26c5d9d62000972c562893147114a01e61
falhassan3 <fareedalhassan65@gmail.com>
2022-10-21 10:40:34 -0700

    task 2 fix 2 debugginging project

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,15 +14,15 @@ int largest;
 
 if (a > b && a > c)
 {
-        largest = a;
+	largest = a;
 }
 else if (b > c && b > a)
 {
-        largest = b;
+	largest = b;
 }
 else if (c > b)
 {
-        largest = c;
+	largest = c;
 }
 else
 {
